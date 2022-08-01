aef28205a995b4f9559a89e4afa39aea49ed3c50
Lautaro Molina <71042127+L-Molina@users.noreply.github.com>
2022-08-01 10:37:25 -0300

    Desafio Clase 12

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
+
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+const messages = [
+    { author: "Juan", text: "¡Hola! ¿Que tal?" },
+    { author: "Pedro", text: "¡Muy bien! ¿Y vos?" },
+    { author: "Ana", text: "¡Genial!" }
+];
+
+//middleware
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname })
+});
+
+httpServer.listen(8080, () => console.log("Server listening on port 8080"));
+
+io.on('connection',socket => {
+    console.log('Un cliente se ha conectado');
+    socket.emit('messages', messages);
+
+    socket.on('new-message',data => {
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+    });
+});
